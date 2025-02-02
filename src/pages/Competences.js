22b4654bef94123c11e7dee030a7bf660d5b4a94
12306672 <https://gitlab.sorbonne-paris-nord.fr/12306672>
2025-02-02 22:18:07 +0100

    added some content to skills's page

--- src/pages/Competences.js
+++ src/pages/Competences.js
@@ -11,12 +11,38 @@ const Competences = () => {
       </div>
       <div className="Content">
         <h1>Compétences</h1>
-        <h2>Le Code</h2>
-        <p>
-          Grâce à ma première année en informatique, j'ai pu apprendre le language de code Golang, Java, Javascript et me
-          perfectionner en Python. J
-        </p>
+        <h2>La programmation</h2>
+        <ul>
+          <li>python</li>
+          <li>web (HTML/CSS)</li>
+          <li>javascript (React Native)</li>
+          <li>PHP (MVC)</li>
+          <li>golang</li>
+        </ul>
         <br />
+        <h2>Gestion de bases de données</h2>
+        <p>non-relationnels</p>
+        <ul>
+          <li>MongoDB</li>
+          <li>Redis</li>
+        </ul>
+        <p>relationnels</p>
+        <ul>
+          <li>SQLite</li>
+        </ul>  
+        <br />
+        <h2>Test automatique</h2>
+          <ul>
+            <li>Jenkins</li>
+          </ul>
+        <br />
+        <h2>Bureautique</h2>
+        <ul>
+          <li>Open Office</li>
+          <li>Google Drive (Docs, Sheets, Slides)</li>
+          <li>Lucidchart</li>
+          <li>Canva</li>
+        </ul>
       </div>
     </div>
   );
